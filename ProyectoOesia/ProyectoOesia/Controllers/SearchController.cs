181c16a4b739e59d52dbd8071929398f62ce5de7
Markel <markel2000es@yahoo.es>
2020-12-05 10:21:09 +0100

    wip

--- ProyectoOesia/ProyectoOesia/Controllers/SearchController.cs
+++ ProyectoOesia/ProyectoOesia/Controllers/SearchController.cs
@@ -1,7 +1,9 @@
 ﻿using Data;
+using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,10 +24,11 @@ namespace ProyectoOesia.Controllers
             _logger = logger;
 
         }
+
         [HttpGet]
-        public async Task<IActionResult> SearchWorkers(string search)
+        public async Task<IActionResult> SearchCompany(string search)
         {
-            var data = await _context.Activity.Where(x => x.Description.Contains(search)).ToListAsync();
+            var data = await _context.Companies.Where(x => x.Description.Contains(search)).ToListAsync();
             if (data.Count != 0)
             {
                 return Ok(data);
@@ -33,11 +36,47 @@ namespace ProyectoOesia.Controllers
             }
             else
             {
-                return NotFound("NO EXISTE NA!");
+                return NotFound("NO EXISTE");
             }
 
 
         }
+        [HttpGet("company")]
+        public async Task<IActionResult> maxDistance(int? id)
+        {
+
+            var data = await _context.Companies.Include(x => x.MaxDistance).FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(data);
+        }
+        [HttpGet("company_uno")]
+        public async Task<IActionResult> fullTime(bool? disponible)
+        {
+            var data = _context.Companies.Include(u => u.Availability);
+
+            List<Company> companys = await _context.Companies.OrderBy(u => u.Availability).ToListAsync();
+
+
+            if (disponible == true)
+            {
+                return Ok(companys);
+            }
+            //if (disponible == true)
+            //{
+            //    companys = companys.Where(x => x.Activity == fullTime).ToList();
+            //}
+
+            else
+            {
+                //companys = companys.Where(x => x.Activity != fullTime).ToList();
+                //return Ok(companys);
+                return Ok(companys);
+            }
+
+
+
+
+        }
+
 
     }
 }
